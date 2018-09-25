b9c02f4c95c7c42709f7d7d76759c6462c558aeb
TangWei <toby911115@163.com>
2018-09-25 17:37:15 +0800

    WxUserInfo.java添加序列化ID

--- src/main/java/cn/net/pwai/mps/project/entity/WxUserInfo.java
+++ src/main/java/cn/net/pwai/mps/project/entity/WxUserInfo.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Entity
 @Table
 public class WxUserInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GenericGenerator(name = "uuid", strategy = "uuid")
